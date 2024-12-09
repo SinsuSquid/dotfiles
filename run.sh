6e42004151eff6f856eff1de9d21d7681400d720
SinsuSquid <105333419+SinsuSquid@users.noreply.github.com>
2024-12-09 12:32:07 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+# .bashrc
+echo "cat ./.bashrc >> ${HOME}/.bashrc" 
 cat ./.bashrc >> ${HOME}/.bashrc
+echo "source ${HOME}/.bashrc"
+source ${HOME}/.bashrc
+
+# .vimrc
+echo "cp ./.vimrc ${HOME}/.vimrc"
 cp ./.vimrc ${HOME}/.vimrc
+
+# .tmux.conf
+echo "cp ./.tmux.conf ${HOME}/.tmux.conf"
 cp ./.tmux.conf ${HOME}/.tmux.conf
+echo "cp ./.tmux.nord ${HOME}/.tmux.nord"
 cp ./.tmux.nord ${HOME}/.tmux.nord
 
 echo "Done! >:D"
