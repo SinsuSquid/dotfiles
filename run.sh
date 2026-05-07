4c97796f1c49fa9e1fe9df1d2ec9dcc1c9d58dd4
SinsuSquid <105333419+SinsuSquid@users.noreply.github.com>
2026-05-07 14:03:36 +0900

    Enhance run.sh with additional installations
    Added installation steps for oh-my-bash, vim-plug, and MAGI-theme.

--- run.sh
+++ run.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 
+# Oh my bash
+echo "Installing oh-my-bash"
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+
 # .bashrc
 echo "cat ./.bashrc >> ${HOME}/.bashrc" 
 cat ./.bashrc >> ${HOME}/.bashrc
 echo "source ${HOME}/.bashrc"
 source ${HOME}/.bashrc
 
+# .tmux.conf
+echo "cat ./.tmux.conf >> ${HOME}/.tmux.conf" 
+cat ./.tmux.conf >> ${HOME}/.tmux.conf
+
+# vim-plug
+echo "Installing vim-plug"
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    
 # .vimrc
 echo "cp ./.vimrc ${HOME}/.vimrc"
 cp ./.vimrc ${HOME}/.vimrc
 
-# .tmux.conf
-echo "cp ./.tmux.conf ${HOME}/.tmux.conf"
-cp ./.tmux.conf ${HOME}/.tmux.conf
-echo "cp ./.tmux.dracula -p ${HOME}/.config/tmux.dracula"
-cp ./.tmux.dracula -p ${HOME}/.config/tmux.dracula
+# Magi Theme
+echo "Installing MAGI-theme"
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/SinsuSquid/MAGI-theme/main/install.sh)"
 
 echo "Done! >:D"
